446222e631ff4f55b5a9e42d897b321745ca5827
forkanimahdi <forkanimahdi@gmail.com>
2023-07-14 07:52:08 +0000

    Home component save 1

--- src/pages/Home/components/HomeComponent.js
+++ src/pages/Home/components/HomeComponent.js
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
 import './HomeComponent.sass';
 
 export const HomeBody = (props) => {
 
+    let date = new Date()
+    let hours = date.getHours()
+    let [dayPeriod, setDayPeriod] = useState('');
+    useEffect(() => {
+            dayPeriod = hours < 12 ? 'mornning' : hours < 18 ? 'afternoon' : "evenning",
+            setDayPeriod(dayPeriod);
+        }, [hours])
 
     return (
         <>
@@ -9,6 +17,10 @@ export const HomeBody = (props) => {
             <div className="homebody">
                 <div className="home-welcoming-header">
                     <h4 className='text-white'>welcome <span className=''>@{props.onlineUser.username}</span></h4>
+                </div>
+                <h4>Good {dayPeriod}</h4>
+                <div className="container posting-area">
+
                 </div>
 
 
